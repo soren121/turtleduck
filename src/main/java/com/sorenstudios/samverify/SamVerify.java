50e3f95ddfd781aa3582bbc9cd588707b8eb40ba
soren121 <soren121@sorenstudios.com>
2015-02-05 20:13:20 -0500

    It does something now.

--- src/main/java/com/sorenstudios/samverify/SamVerify.java
+++ src/main/java/com/sorenstudios/samverify/SamVerify.java
@@ -1,7 +1,25 @@
 package com.sorenstudios.samverify;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public final class SamVerify extends JavaPlugin {
-	// <Fluttershy> Yay
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("register")) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage("This command can only be run by a player.");
+            }
+            else {
+                Player player = (Player) sender;
+                player.sendMessage("Fluttershy says hello.");
+            }
+            
+            return true;
+        }
+        
+        return false;
+    }
 }
