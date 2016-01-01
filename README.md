86760ccbb700c995b7ad708ec613d23e13b2b317
Nicholas Narsing <soren121@sorenstudios.com>
2016-01-01 04:19:31 -0500

    Update project for new stuff; add LGPLv3

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 # turtleduck
-[![Build Status](https://travis-ci.org/soren121/turtleduck.svg?branch=master)](https://travis-ci.org/soren121/turtleduck)
 
-This is a plugin/bot that allows users on a Spigot 1.8+ server to immediately contact the admins through Telegram.
+Turtleduck is a [Telegram bot](https://core.telegram.org/bots) that is designed to allow 
+players on a Minecraft server to contact the admins. Strictly speaking, it can work 
+independently of a Minecraft server, but some functionality only works with one.
 
-Currently in pre-alpha. It doesn't do much. Or anything, really.
+Turtleduck is comprised of two parts: a PHP component that runs on a webserver which handles 
+communication with the Telegram server and logging, and a Java plugin for the Spigot MC 
+server that allows messages to be sent to a Telegram group via an in-game command.
+
+Runtime requirements:
+ * A web server with PHP 5.6+ and a recent version of MySQL/MariaDB.
+ * Java JRE 8u40+.
+ * Spigot 1.8+ with RCON enabled. (Tested with 1.8.8 only.)
+
+Compile requirements:
+ * Java JDK 8u40+.
+ * Maven 3.2+.
+
+### License
+
+Licensed under the GNU LGPL v3 (or, at your option, any later version.) See LICENSE.txt for more info.
--- src/main/java/com/sorenstudios/turtleduck/Turtleduck.java
+++ src/main/java/com/sorenstudios/turtleduck/Turtleduck.java
@@ -1,12 +1,29 @@
+/*
+    Turtleduck, a Telegram notification bot designed for Minecraft servers
+    Copyright (C) 2016 Nicholas Narsing <soren121@sorenstudios.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published 
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package com.sorenstudios.turtleduck;
 
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Player;
 
-public final class Turtleduck extends JavaPlugin {
+public class Turtleduck extends JavaPlugin {
+    
     @Override
     public void onEnable() {
     	// Save config if it isn't there already
@@ -15,18 +32,18 @@ public final class Turtleduck extends JavaPlugin {
     	FileConfiguration config = this.getConfig();
 		
         getLogger().info("Loaded successfully!");
+        
+        CommandExecutor sendMessage = (sender, cmd, label, args) -> {
+            if(args.length > 0) {
+                sender.sendMessage("<Fluttershy> yay");
+                return true;
+            }
+            return false;
+        };
+        
+        getCommand("sos").setExecutor(sendMessage);
+        getCommand("batsignal").setExecutor(sendMessage);
+        getCommand("brrng").setExecutor(sendMessage);
     }
     
-    @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        String command = cmd.getName();
-        if ((command.equalsIgnoreCase("sos") || command.equalsIgnoreCase("batsignal")) && args.length > 0) {
-            Player player = (Player) sender;
-            player.sendMessage("<Fluttershy> yay");
-            
-            return true;
-        }
-        
-        return false;
-    }
 }
